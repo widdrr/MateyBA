39d1e6449fe43cf37b603fa9be0b92aba0299530
Petrica81 <95485582+Petrica81@users.noreply.github.com>
2023-05-01 18:01:03 +0300

    Rewards implemented on death

--- Assets/Death.cs
+++ Assets/Death.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class Death : StateMachineBehaviour
 {
+    public Inventory playerInventory;
     // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
     //override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     //{
@@ -19,9 +20,26 @@ public class Death : StateMachineBehaviour
     // OnStateExit is called when a transition ends and the state machine finishes evaluating this state
     override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
-      animator.gameObject.SetActive(false);
+        GenerateLoot();
+        animator.gameObject.SetActive(false);
     }
 
+    private void GenerateLoot()
+    {
+        int auxChance = Random.Range(1, 11);
+        if (auxChance >= 5 && auxChance <= 8)
+        {
+            playerInventory.coins += 1;
+        }
+        else if (auxChance >= 9)
+        {
+            playerInventory.coins += 2;
+        }
+        if (auxChance == 3)
+        {
+            playerInventory.potions += 1;
+        }
+    }
     // OnStateMove is called right after Animator.OnAnimatorMove()
     //override public void OnStateMove(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     //{
