8ff7435e4ce93d697ac0ea655d87a423f7def9e6
Petrica81 <95485582+Petrica81@users.noreply.github.com>
2023-05-05 15:04:57 +0300

    glock animation & weapon swap

---
+++ Assets/Scripts/Canvas/WeaponSpriteManager.cs
@@ -0,0 +1,75 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class WeaponSpriteManager : MonoBehaviour
+{
+    public Image rightImage;
+    public Image leftImage;
+    public Inventory inventory;
+
+    public void Start()
+    {
+        if (inventory.rightWeapon != null) 
+        {
+            rightImage.sprite = inventory.rightWeapon.weaponSprite;
+            rightImage.color = Color.white;
+        }
+        else
+        {
+            rightImage.sprite = null;
+            rightImage.GetComponent<Graphic>().color = new Color32(154, 120, 65, 255);
+        }
+        if (inventory.leftWeapon != null)
+        {
+            leftImage.sprite = inventory.leftWeapon.weaponSprite;
+            leftImage.color = Color.white;
+        }
+        else
+        {
+            leftImage.sprite = null;
+            leftImage.GetComponent<Graphic>().color = new Color32(154, 120, 65, 255);
+        }
+    }
+    public void ChangeRightWeapon(Weapon newweapon)
+    {
+        if(inventory.rightWeapon && inventory.rightWeapon.size == WeaponSize.doubleSlot)
+        {
+            inventory.leftWeapon = null;
+            leftImage.sprite = null;
+            leftImage.GetComponent<Graphic>().color = new Color32(154, 120, 65, 255);
+        }
+
+        rightImage.sprite = newweapon.weaponSprite;
+        rightImage.color = Color.white;
+        inventory.rightWeapon = newweapon;
+
+        if(newweapon.size == WeaponSize.doubleSlot)
+        {
+            leftImage.sprite = newweapon.weaponSprite;
+            inventory.leftWeapon = newweapon;
+            leftImage.color = Color.white;
+        }
+    }
+    public void ChangeLeftWeapon(Weapon newweapon) 
+    { 
+        if(inventory.leftWeapon && inventory.leftWeapon.size == WeaponSize.doubleSlot)
+        {
+            inventory.rightWeapon = null;
+            rightImage.sprite = null;
+            rightImage.GetComponent<Graphic>().color = new Color32(154, 120, 65, 255);
+        }
+
+        leftImage.sprite = newweapon.weaponSprite;
+        inventory.leftWeapon = newweapon;
+        leftImage.color = Color.white;
+
+        if(newweapon.size == WeaponSize.doubleSlot)
+        {
+            rightImage.sprite = newweapon.weaponSprite;
+            inventory.rightWeapon = newweapon;
+            rightImage.color = Color.white;
+        }
+    }
+}
--- Assets/Scripts/ScriptableObjects/Weapon.cs
+++ Assets/Scripts/ScriptableObjects/Weapon.cs
@@ -7,12 +7,19 @@ public enum WeaponType
     glock,
     crowbar
 }
+public enum WeaponSize
+{
+    singleSlot,
+    doubleSlot
+}
 
 [CreateAssetMenu(fileName = "NewWeapon", menuName = "Scriptable Objects/Weapon")]
 public class Weapon : ScriptableObject
 {
     public string weaponName;
+    public Sprite weaponSprite;
     public WeaponType type;
+    public WeaponSize size;
     public int damage;
     public float waitingTime = 0.32f;
 
