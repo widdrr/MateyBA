50b86ada4d0e881fa8c21bee6748622a87ee52e4
Petrica81 <95485582+Petrica81@users.noreply.github.com>
2023-05-31 22:40:35 +0300

    Cars

---
+++ Assets/Scripts/Cars/CarMovement.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarMovement : MonoBehaviour
+{
+    private Vector3 direction;
+    private float speed;
+    private Animator animator;
+
+    public void InitializeMovement(Vector3 direction, float speedAux)
+    {
+        this.direction = direction.normalized;
+        speed = speedAux;
+        animator = GetComponent<Animator>();
+        animator.SetFloat("moveX", direction.x);
+    }
+
+    private void Update()
+    {
+        MoveCar();
+    }
+    private void MoveCar()
+    {
+        transform.Translate(speed * Time.deltaTime * direction);
+    }
+
+    public void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.gameObject.tag == "destroyer")
+            Destroy(gameObject);
+
+    }
+}
+
---
+++ Assets/Scripts/Cars/CarSpawner.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarSpawner : MonoBehaviour
+{
+    public GameObject[] carPrefabs;
+    public Vector3 direction;
+    public float speed;
+
+    private void Start()
+    {
+        InvokeRepeating("SpawnCar", 0f, 5f);
+    }
+    private void SpawnCar()
+    {
+        int randomIndex = Random.Range(0, carPrefabs.Length);
+        GameObject newCar = Instantiate(carPrefabs[randomIndex], transform.position, Quaternion.identity);
+        CarMovement carMovement = newCar.GetComponent<CarMovement>();
+        carMovement.InitializeMovement(direction, speed);
+    }
+}
