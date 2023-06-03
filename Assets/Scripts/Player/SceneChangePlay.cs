7ff2cb3b892543d7c1af68ff94a83c207be70164
Andrei Tava <tavaandrei@gmail.com>
2023-06-03 19:37:08 +0300

    basic restart on death

--- Assets/Scripts/Player/SceneChangePlay.cs
+++ Assets/Scripts/Player/SceneChangePlay.cs
@@ -5,8 +5,28 @@ using UnityEngine.SceneManagement;
 
 public class SceneChangePlay : MonoBehaviour
 {
+    public void Start()
+    {
+        SetRatio(4, 3);   
+    }
+    void SetRatio(float w, float h)
+    {
+        if ((((float)Screen.width) / ((float)Screen.height)) > w / h)
+        {
+            Screen.SetResolution((int)(((float)Screen.height) * (w / h)), Screen.height, true);
+        }
+        else
+        {
+            Screen.SetResolution(Screen.width, (int)(((float)Screen.width) * (h / w)), true);
+        }
+    }
     public void LoadScene(string sceneName)
     {
 	    SceneManager.LoadScene(sceneName);
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
     }
 }
