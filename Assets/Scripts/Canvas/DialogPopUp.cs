83cf16f03b39da65e6379b2d7942882aab81f2d9
Petrica81 <95485582+Petrica81@users.noreply.github.com>
2023-06-04 00:11:55 +0300

    NPC

---
+++ Assets/Scripts/Canvas/DialogPopUp.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogPopUp : MonoBehaviour
+{
+    public GameObject window;
+    public void OnTriggerEnter2D(Collider2D collider)
+    {
+        window.SetActive(true);
+    }
+    public void OnTriggerExit2D(Collider2D collision)
+    {
+        window.SetActive(false);
+    }
+}
