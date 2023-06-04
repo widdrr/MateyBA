a7e4ec29b5918b6a3aab902ca824d42f6e68a274
Roșianu Robert <94984223+RobertKeanu@users.noreply.github.com>
2023-06-04 22:14:23 +0300

    End screen / restart screen (#84)
    * End screen / restart screen
    
    tava will kill me after this one
    
    * Fixed errors
    
    bully lord

---
+++ Assets/Scripts/CommonComponents/EndGame.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class EndGame : MonoBehaviour
+{
+    public HealthManager player;
+    public void Start()
+    {
+     	HealthManager player = GameObject.FindWithTag("Player").GetComponent<HealthManager>();   
+    }
+    public void FixedUpdate()
+    {
+		if(player.maxHealth > 12)
+	{
+	    	SceneManager.LoadScene("CreditScene");
+	}
+    }
+}
--- Assets/Scripts/CommonComponents/HealthManager.cs
+++ Assets/Scripts/CommonComponents/HealthManager.cs
@@ -29,4 +29,5 @@ public class HealthManager : MonoBehaviour, IOnHitSubscriber
         
         CurrentHealth += value;
     }
+
 }
