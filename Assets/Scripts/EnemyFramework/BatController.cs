0f6e7a3ad8072f8d13811ba1f61500525665344a
Roșianu Robert <94984223+RobertKeanu@users.noreply.github.com>
2023-12-19 03:00:14 +0200

    Add enemy 3 (#26)

---
+++ Assets/Scripts/EnemyFramework/BatController.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace EnemyFramework
+{
+    public class BatController : GenericEnemyController
+    {
+        [SerializeField]
+        private float _alertRadius = 0.2f;
+
+        [SerializeField]
+        private Vector2 _movementDirection;
+        protected new void Start()
+        {
+            base.Start();
+            attackingDirection = Vector2.Perpendicular(_movementDirection).normalized;
+            target = GameObject.FindWithTag("Player").transform;
+        }
+        protected override void AttackSequence()
+        {
+            return;
+        }
+
+        protected override bool ConditionIsSatisfied()
+        {
+            return false;
+        }
+        protected override void IdleBehaviour()
+        {
+            if (currentState == EnemyState.idle || currentState == EnemyState.moving)
+            {
+                movementDirection = (target.transform.position - transform.position);
+
+                if (movementDirection.magnitude > _alertRadius)
+                    return;
+
+                movementDirection = movementDirection.normalized;
+                currentState = EnemyState.moving;
+                enemyRigidbody.MovePosition(transform.position + speed * Time.deltaTime * movementDirection);
+
+            }
+        }
+    }
+}
